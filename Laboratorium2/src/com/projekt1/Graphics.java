53086dafcd9d5968f8931393ae886155787a6bdc
klaudiaa111 <33720692+klaudiaa111@users.noreply.github.com>
2018-11-26 23:53:09 +0100

    Add files via upload

--- Laboratorium2/src/com/projekt1/Graphics.java
+++ Laboratorium2/src/com/projekt1/Graphics.java
@@ -5,13 +5,18 @@ import com.projekt1.Double;
 import com.projekt1.Str;
 import com.projekt1.Value;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
@@ -20,12 +25,15 @@ import javafx.stage.Stage;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Graphics extends Application {
     private Text actionStatus;
     private Stage savedStage;
     DataFrame d;
     File selectedFile;
+    String[] arr;
+
 
     public static void main(String[] args) {
         Application.launch(args);
@@ -34,30 +42,41 @@ public class Graphics extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button open = new Button("Open");
-        HBox open1 = new HBox(10);
-        open1.getChildren().addAll(open);
+
         Button min = new Button("Min");
-        HBox min1 = new HBox(10);
-        min1.getChildren().addAll(min);
+
         Button max = new Button("Max");
-        HBox max1 = new HBox(10);
-        max1.getChildren().addAll(max);
+
         Button sum = new Button("Sum");
-        HBox sum1 = new HBox(10);
-        sum1.getChildren().addAll(sum);
+
         Button mean = new Button("Mean");
-        HBox mean1 = new HBox(10);
-        mean1.getChildren().addAll(mean);
 
+        Button var = new Button("Var");
 
+        Button std = new Button("Std");
 
+        Label name = new Label("Groupby:");
+        CheckBox cb1 = new CheckBox("id");
+        CheckBox cb2 = new CheckBox("date");
+        CheckBox cb3 = new CheckBox("total");
+        CheckBox cb4 = new CheckBox("val");
+        Button ok = new Button("OK");
+        VBox checkbox = new VBox(30);
+        checkbox.getChildren().addAll(name, cb1, cb2, cb3, cb4, ok);
+
+        Button create = new Button("Create a chart");
+        StackPane cr = new StackPane();
+        cr.getChildren().add(create);
 
 
         actionStatus = new Text();
-        VBox vbox = new VBox(30);
-        Label wynikLabel = new Label("Wynik:");
+        VBox buttons = new VBox(30);
+        buttons.getChildren().addAll(min, max, sum, mean, var, std, actionStatus);
+
+        VBox result = new VBox(30);
+        Label resultLabel = new Label("Wynik:");
         // Dodajemy ją do VBox-u
-        vbox.getChildren().add(wynikLabel);
+        result.getChildren().add(resultLabel);
         // Tworzymy TextArea do wypisywania wyniku
         TextArea wynikTextArea = new TextArea();
         // Wyłączamy możliwość edycji
@@ -65,16 +84,115 @@ public class Graphics extends Application {
         // Włączamy zawijanie tekstu
         wynikTextArea.setWrapText(true);
         // Dodajemy do VBox-u
-        vbox.getChildren().add(wynikTextArea);
+        result.getChildren().add(wynikTextArea);
 
+        HBox hbox = new HBox(100);
+        hbox.getChildren().addAll(checkbox, buttons);
 
+        VBox all = new VBox(30);
+        all.getChildren().addAll(open, hbox, cr, result);
 
-
-        vbox.getChildren().addAll( open1,min1, max1, sum1, mean1, actionStatus);
-        Scene scene = new Scene(vbox, 800, 800);
+        Scene scene = new Scene(all, 800, 850);
         primaryStage.setScene(scene);
         primaryStage.show();
         savedStage = primaryStage;
+
+        //second stage
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+
+        LineChart lineChart = new LineChart(xAxis, yAxis);
+        ChoiceBox choice1 = new ChoiceBox(FXCollections.observableArrayList(
+                "id", "date", "total", "val")
+        );
+        ChoiceBox choice2 = new ChoiceBox(FXCollections.observableArrayList(
+                "id", "date", "total", "val")
+        );
+        Button draw = new Button("Draw!");
+        HBox hb = new HBox(30);
+        hb.getChildren().addAll(choice1, choice2);
+
+        VBox all2 = new VBox(30);
+        all2.getChildren().addAll(hb, draw);
+        lineChart.setTitle("Stock Monitoring, 2010");
+        //defining a series
+
+
+        Scene secondScene = new Scene(all2, 230, 100);
+
+        // New window (Stage)
+        Stage newWindow = new Stage();
+        newWindow.setTitle("Second Stage");
+        newWindow.setScene(secondScene);
+
+        // Set position of second window, related to primary window.
+        newWindow.setX(primaryStage.getX() + 200);
+        newWindow.setY(primaryStage.getY() + 100);
+
+        ok.setOnAction(new EventHandler<ActionEvent>() {
+
+            List<String> group = new ArrayList<String>();
+            @Override
+            public void handle(ActionEvent event) {
+                if(cb1.isSelected()){
+                    group.add("id");
+                }
+                else if(cb2.isSelected()){
+                    group.add("date");
+                }
+                else if(cb3.isSelected()){
+                    group.add("total");
+                }
+                else if(cb4.isSelected()){
+                    group.add("val");
+                }
+                arr = new String[group.size()];
+                group.toArray(arr);
+
+            }
+        });
+
+        draw.setOnAction(new EventHandler<ActionEvent>(){
+
+            @Override
+            public void handle(ActionEvent event) {
+                newWindow.setTitle("Chart");
+                //defining the axes
+
+                final CategoryAxis xAxis = new CategoryAxis();
+                final CategoryAxis yAxis = new CategoryAxis();
+                xAxis.setLabel(choice1.getValue().toString());
+                yAxis.setLabel(choice2.getValue().toString());
+                //creating the chart
+                final LineChart<String, String> lineChart = new LineChart<String, String>(xAxis,yAxis);
+
+                lineChart.setTitle("Chart");
+                //defining a series
+                XYChart.Series series = new XYChart.Series();
+                series.setName("Points");
+                //populating the series with data
+                for(int i=0; i<d.size(); i++){
+                    series.getData().add(new XYChart.Data (d.get(choice1.getValue().toString()).get(i).toString(), d.get(choice2.getValue().toString()).get(i).toString()));
+                }
+
+                Scene scene  = new Scene(lineChart,800,600);
+                lineChart.getData().add(series);
+
+                newWindow.setScene(scene);
+                newWindow.show();
+            }
+        });
+
+        create.setOnAction(new EventHandler<ActionEvent>(){
+
+            @Override
+            public void handle(ActionEvent event) {
+
+
+                newWindow.show();
+            }
+        });
+
 
         open.setOnAction(new EventHandler<ActionEvent>() {
 
@@ -106,7 +224,7 @@ public class Graphics extends Application {
                             @Override
                             public void handle(ActionEvent event) {
                                 wynikTextArea.setText("====Min====" + "\n");
-                                DataFrame df = d.groupby("id").min();
+                                DataFrame df = d.groupby(arr).min();
                                 for (String col : df.colNames) {
                                     wynikTextArea.appendText(col + "   ||   ");
                                 }
@@ -134,7 +252,7 @@ public class Graphics extends Application {
             @Override
             public void handle(ActionEvent event) {
                 wynikTextArea.setText("====Max====" + "\n");
-                DataFrame df = d.groupby("id").max();
+                DataFrame df = d.groupby(arr).max();
                 for (String col : df.colNames) {
                     wynikTextArea.appendText(col + "   ||   ");
                 }
@@ -162,7 +280,7 @@ public class Graphics extends Application {
             @Override
             public void handle(ActionEvent event) {
                 wynikTextArea.setText("====Mean====" + "\n");
-                DataFrame df = d.groupby("id").mean();
+                DataFrame df = d.groupby(arr).mean();
                 for (String col : df.colNames) {
                     wynikTextArea.appendText(col + "   ||   ");
                 }
@@ -189,7 +307,62 @@ public class Graphics extends Application {
             @Override
             public void handle(ActionEvent event) {
                 wynikTextArea.setText("====Sum====" + "\n");
-                DataFrame df = d.groupby("id").sum();
+                DataFrame df = d.groupby(arr).sum();
+                for (String col : df.colNames) {
+                    wynikTextArea.appendText(col + "   ||   ");
+                }
+
+                wynikTextArea.appendText("\n");
+
+                for (int i = 0; i < df.get(df.colNames[0]).size(); i++) {
+                    for (int j = 0; j < df.colNames.length; j++) {
+
+                        ArrayList<Value> databaseRow = df.get(df.colNames[j]);
+                        Value obj = databaseRow.get(i);
+                        String o = obj.toString();
+                        Class<? extends Value> dataType = df.colType.get(df.colNames[j]);
+
+                        wynikTextArea.appendText(o + "      ||      ");
+                    }
+                    wynikTextArea.appendText("\n");
+
+                }
+            }
+        });
+        var.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                wynikTextArea.setText("====Var====" + "\n");
+                DataFrame df = d.groupby(arr).var();
+                for (String col : df.colNames) {
+                    wynikTextArea.appendText(col + "   ||   ");
+                }
+
+                wynikTextArea.appendText("\n");
+
+                for (int i = 0; i < df.get(df.colNames[0]).size(); i++) {
+                    for (int j = 0; j < df.colNames.length; j++) {
+
+                        ArrayList<Value> databaseRow = df.get(df.colNames[j]);
+                        Value obj = databaseRow.get(i);
+                        String o = obj.toString();
+                        Class<? extends Value> dataType = df.colType.get(df.colNames[j]);
+
+                        wynikTextArea.appendText(o + "      ||      ");
+                    }
+                    wynikTextArea.appendText("\n");
+
+                }
+            }
+        });
+
+        std.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                wynikTextArea.setText("====Std====" + "\n");
+                DataFrame df = d.groupby(arr).std();
                 for (String col : df.colNames) {
                     wynikTextArea.appendText(col + "   ||   ");
                 }
